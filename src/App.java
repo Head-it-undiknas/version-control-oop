00006fe892969453b7719ee4f3bd880b0de84da2
humadev <huma.elektro@gmail.com>
2021-11-04 08:44:24 +0800

    tambah nama

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        Hero hero1 = new Hero("Huma");
+        Hero hero1 = new Hero("Huma prathama");
 
         System.out.println(hero1.moveForward());
         System.out.println(hero1.moveBackward());
